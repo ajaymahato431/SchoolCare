240445e917497c2fadd6be6ea165acc148449c92
ajaymahato431 <mahatoajay9988@gmail.com>
2025-01-23 08:43:47 +0545

    finishing

--- app/Filament/Student/Resources/StudentResource/Pages/ViewStudent.php
+++ app/Filament/Student/Resources/StudentResource/Pages/ViewStudent.php
@@ -4,6 +4,7 @@ namespace App\Filament\Student\Resources\StudentResource\Pages;
 
 use App\Filament\Student\Resources\StudentResource;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewStudent extends ViewRecord
@@ -13,7 +14,11 @@ class ViewStudent extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            //
+            Action::make('Download Report')
+                ->url(fn($record) => route('studentReport.pdf', $record->id))
+                ->openUrlInNewTab()
+                ->label('Download Report')
+                ->icon('heroicon-o-arrow-down-tray'),
         ];
     }
 }
